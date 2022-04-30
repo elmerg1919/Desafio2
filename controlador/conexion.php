65d286bbc6a6b4aeeed0063c4c86ad7daebceb47
Marlon Stanley Realegeño Duran RD162798 <88696537+MarlonRealegeno@users.noreply.github.com>
2022-04-30 00:33:08 -0600

    Add files via upload

--- controlador/conexion.php
+++ controlador/conexion.php
@@ -1,1 +1,28 @@
+<?php
 
+class Conection{
+
+    public function _getConection(){
+
+        
+
+        //SERVIDOR LOCALHOST
+        $user = "root";
+        $pass = "";
+        $host = "localhost";
+        $db = "eventos";
+        
+        try{
+            $conectar = new PDO("mysql:host=$host;dbname=$db",$user,$pass);
+            //echo "Conexion lograda"; 
+        }catch(PDOException $e){
+            echo $e->getMessage(); 
+           // echo "error al conectar";
+        }
+        
+        
+        return $conectar;
+    }
+}
+
+?>
